fb8798ae30cfe2148f1e2184cbd7b74d08cadddb
JustinHockett <15hockettj@gmail.com>
2021-10-12 10:47:49 -0700

    update pipeline

--- code/Coding Demo AW.R
+++ code/Coding Demo AW.R
@@ -7,3 +7,35 @@
 rm(list=ls())
 graphics.off()
 
+
+# generate some data ------------------------------------------------------
+
+n<- 30
+m<- 2.2
+b<- 1.1
+
+x<- rnorm(n=n,mean= 0,sd=1)   #indep. var
+z<- rnorm(n=n,mean= 0,sd=1)   #noise
+y<- m * x + b + z             #dep. var
+
+plot(x,y) #debug plot
+
+
+
+# Do linear regression ----------------------------------------------------
+
+fit<- glm(y ~ x)
+
+
+
+# Plot the results --------------------------------------------------------
+
+par(mar=c(6,7,4,1))
+plot(x,y,
+     xlab="Normalized velocity",
+     ylab="Normalized\nenergy consumption"
+     )
+abline(fit)
+
+
+
